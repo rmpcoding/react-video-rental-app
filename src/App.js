f4fd5c94d42eb0d100b9b04a5d4fdff94570fe75
Robert Parsons <rmpcoding@gmail.com>
2020-10-22 17:23:15 -0700

    Adds state to App.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+    state = {
+      initial: 0
+    };
+
+    render() {
+      return (
+        <div className="App">
+          <span>{this.state.initial}</span>
+          <button>+</button>
+          <button>-</button>
+        </div>
+      );
+    }
 }
 
 export default App;

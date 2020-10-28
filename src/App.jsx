eda0929e7970ae4881995f08159ab66f8dbe3c8f
Robert Parsons <rmpcoding@gmail.com>
2020-10-28 15:48:44 -0700

    Adds mapped out list items for dynamic listing

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,32 @@ import './App.css';
 class App extends Component {
     state = {
         count: 5,
+        tags: ['tag1', 'tag2', 'tag3'],
     };
-    
-    style = (this.state.count === 0)
-        ? ({
-              backgroundColor: 'red',
-          })
-        : ({
-              backgroundColor: 'blue',
-          });
+
+    style =
+        this.state.count === 0
+            ? {
+                  backgroundColor: 'red',
+              }
+            : {
+                  backgroundColor: 'blue',
+              };
 
     render() {
         return (
             <>
                 <div className={'div-count-wrapper'}>
-                    <span className={'count'} style={this.style}>{this.countFunction()}</span>
+                    <span className={'count'} style={this.style}>
+                        {this.countFunction()}
+                    </span>
                     <button>+</button>
                     <button>-</button>
+                    <ul>
+                        {this.state.tags.map((tag) => {
+                            return <li className="text" key={tag}>{tag}</li>;
+                        })}
+                    </ul>
                 </div>
             </>
         );

18363f1ed615ee1d981ef1dec818ed2a67c3c123
Robert Parsons <rmpcoding@gmail.com>
2020-10-28 16:20:43 -0700

    Adds binding of this to click handler function for incrementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import './App.css';
 
 class App extends Component {
     state = {
-        count: 5,
+        count: 0,
         tags: ['tag1', 'tag2', 'tag3'],
     };
+
+    constructor() {
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this)
+    }
 
     style =
         this.state.count === 0
@@ -16,6 +21,16 @@ class App extends Component {
                   backgroundColor: 'blue',
               };
 
+    countFunction() {
+        const { count } = this.state;
+        return count === 10 ? 'ten ' : count;
+    }
+
+    handleIncrement() {
+        this.setState({count: this.state.count + 1})
+    }
+
+
     render() {
         return (
             <>
@@ -23,21 +38,20 @@ class App extends Component {
                     <span className={'count'} style={this.style}>
                         {this.countFunction()}
                     </span>
-                    <button>+</button>
+                    <button onClick={this.handleIncrement}>+</button>
                     <button>-</button>
                     <ul>
                         {this.state.tags.map((tag) => {
-                            return <li className="text" key={tag}>{tag}</li>;
+                            return (
+                                <li className="text" key={tag}>
+                                    {tag}
+                                </li>
+                            );
                         })}
                     </ul>
                 </div>
             </>
         );
-    }
-
-    countFunction() {
-        const { count } = this.state;
-        return count === 10 ? 'ten ' : count;
     }
 }
 

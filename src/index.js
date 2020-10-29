1b416f68e2dd0a2f779a27772d949bf3f7c0a626
Robert Parsons <rmpcoding@gmail.com>
2020-10-28 17:20:10 -0700

    imports bootstrap and font-awesome files into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
   <React.StrictMode>

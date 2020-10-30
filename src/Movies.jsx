d0c979d020bac02921c3fa25eecb9f30897c92bb
Robert Parsons <rmpcoding@gmail.com>
2020-10-30 00:09:30 -0700

    Adds movies from services to render onto DOM

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import { getMovies } from './services/fakeMovieService';
 
-
 class Movies extends Component {
+    state = {
+        movies: getMovies(),
+    };
 
+    // handleGetMovies() {
+    //     return this.setState({
+    //         movies: getMovies()
+    //     })
+    // }
 
     render() {
         return (
-            <h1>
-                This is inside the Movies component
-            </h1>
-        )
+            <>
+                <div>
+                    {this.state.movies.map((movie) => {
+                        console.log(movie);
+                        return <p key={movie._id}>{movie._id}</p>
+                    })}
+                </div>
+                <div>
+                    {this.state.movies.map((movie) => {
+                        console.log(movie);
+                        return <p key={movie._id}>{movie.title}</p>
+                    })}
+                </div>
+            </>
+        );
     }
 }
 

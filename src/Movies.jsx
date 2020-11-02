a1a197e41c8a4397374f22b61118ff24c73d87c4
Robert Parsons <rmpcoding@gmail.com>
2020-11-01 23:16:17 -0800

    Adds db table to a stored variable to use in the ternary statement for readability purposes

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -23,82 +23,48 @@ class Movies extends Component {
 
     render() {
         const table = (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Genre</th>
-                        <th scope="col">Stock</th>
-                        <th scope="col">Rate</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.movies.map((movie) => {
-                        return (
-                            <tr key={movie._id}>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td>
-                                    <button
-                                        type="button"
-                                        className="btn btn-danger btn-sm"
-                                        onClick={() => this.handleDelete(movie)}
-                                    >
-                                        Delete
-                                    </button>
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <>
+                <p>Showing {this.state.count} movies in the database</p>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Genre</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Rate</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.movies.map((movie) => {
+                            return (
+                                <tr key={movie._id}>
+                                    <td>{movie.title}</td>
+                                    <td>{movie.genre.name}</td>
+                                    <td>{movie.numberInStock}</td>
+                                    <td>{movie.dailyRentalRate}</td>
+                                    <td>
+                                        <button
+                                            type="button"
+                                            className="btn btn-danger btn-sm"
+                                            onClick={() =>
+                                                this.handleDelete(movie)
+                                            }
+                                        >
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </>
         );
-
         return (
             <>
                 {this.state.count ? (
-                    <>
-                        <p>
-                            Showing {this.state.count} movies in the database
-                        </p>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Genre</th>
-                                    <th scope="col">Stock</th>
-                                    <th scope="col">Rate</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.movies.map((movie) => {
-                                    return (
-                                        <tr key={movie._id}>
-                                            <td>{movie.title}</td>
-                                            <td>{movie.genre.name}</td>
-                                            <td>{movie.numberInStock}</td>
-                                            <td>{movie.dailyRentalRate}</td>
-                                            <td>
-                                                <button
-                                                    type="button"
-                                                    className="btn btn-danger btn-sm"
-                                                    onClick={() =>
-                                                        this.handleDelete(movie)
-                                                    }
-                                                >
-                                                    Delete
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </>
+                    table
                 ) : (
                     <p>We have no movies to show you right now</p>
                 )}

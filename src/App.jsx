891c8f72cbaabfbe40a1330081c1775964f37b66
Robert Parsons <rmpcoding@gmail.com>
2020-11-04 13:44:08 -0800

    Removes unnecessary rendering of the heart icon in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Movies from './Movies';
-import Heart from './components/common/Heart';
 
 class App extends Component {
     render() {
         return (
             <>
                 <main className="container">
-                    <Heart />
                     <Movies />
                 </main>
             </>

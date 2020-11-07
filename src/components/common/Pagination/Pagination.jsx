d3a52fbad09445537ba1d38add2a06314f9d4d24
Robert Parsons <rmpcoding@gmail.com>
2020-11-06 22:26:48 -0800

    Adds basic skeleton to Pagination component

---
+++ src/components/common/Pagination/Pagination.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Pagination extends Component {
+    render() {
+        return(
+            <>
+                <h1>Pagination Goes Here</h1>
+            </>
+        )
+    }
+}
+
+export default Pagination;
